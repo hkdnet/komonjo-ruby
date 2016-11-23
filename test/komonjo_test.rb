aa31fccc93a8c0d10345ddb0d2a7f26228cbb389
hkdnet <hkdnet@users.noreply.github.com>
2016-11-24 01:17:15 +0900

    Remove unnecessary specs

--- test/komonjo_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-describe Komonjo do
-  it 'should has a version number' do
-    assert { ::Komonjo::VERSION.nil? == false }
-  end
-end
--- test/test_helper.rb
+++
@@ -1,6 +0,0 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'komonjo'
-require 'komonjo/mocks/slack_mock'
-
-require 'minitest/autorun'
-require 'minitest-power_assert'
