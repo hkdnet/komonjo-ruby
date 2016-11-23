102a7d05eacf80c7261f36993e578845b7f0868f
hkdnet <hkdnet@users.noreply.github.com>
2016-11-23 23:46:07 +0900

    Extract methods
    This is not so good...

--- lib/komonjo/models/partial_message.rb
+++ lib/komonjo/models/partial_message.rb
@@ -17,30 +17,11 @@ module Komonjo
         f = filters.find { |e| e.match(text) }
         return unless f
         arr = f.parse(text)
+        @text = arr.first[:text]
         case arr.size
-        when 1
-          a, = arr
-          @text = a[:text]
-          extend f.mixin
-        when 2
-          a, b = arr
-          @text = a[:text]
-          if a[:matched]
-            extend f.mixin
-            @next = PartialMessage.new(b[:text], filters)
-            @next.parse
-          else
-            @next = PartialMessage.new(b[:text], filters).tap do |e|
-              e.extend f.mixin
-            end
-          end
-        when 3
-          a, b, c = arr
-          @text = a[:text]
-          @next = PartialMessage.new(b[:text], filters).tap do |e|
-            e.extend f.mixin
-            e.next = PartialMessage.new(c[:text], filters).tap(&:parse)
-          end
+        when 1 then extend f.mixin
+        when 2 then setup_with_two(f, arr)
+        when 3 then setup_with_three(f, arr)
         end
         self
       end
@@ -50,6 +31,27 @@ module Komonjo
       end
 
       def embed(*)
+      end
+
+      private
+
+      def setup_with_two(f, arr)
+        a, b = arr
+        @next = PartialMessage.new(b[:text], filters)
+        if a[:matched]
+          extend f.mixin
+          @next.parse
+        else
+          @next.extend f.mixin
+        end
+      end
+
+      def setup_with_three(f, arr)
+        _, b, c = arr
+        @next = PartialMessage.new(b[:text], filters).tap do |e|
+          e.extend f.mixin
+          e.next = PartialMessage.new(c[:text], filters).tap(&:parse)
+        end
       end
     end
   end
