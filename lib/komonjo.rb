261cc16b565e96582bb9f821cb5b37728026e10c
hkdnet <hkdnet@users.noreply.github.com>
2016-11-26 22:50:16 +0900

    Add config

--- lib/komonjo.rb
+++ lib/komonjo.rb
@@ -1,6 +1,7 @@
 require 'json'
 
 require 'komonjo/extensions/hash_extensions'
+require 'komonjo/config'
 require 'komonjo/client'
 require 'komonjo/version'
 require 'komonjo/connections/slack_connection'
@@ -9,3 +10,15 @@ require 'komonjo/models'
 require 'komonjo/resources'
 require 'komonjo/mocks/slack_mock'
 require 'komonjo/services'
+
+module Komonjo
+  class << self
+    def config
+      @config = Komonjo::Config.new
+    end
+
+    def configure
+      yield config
+    end
+  end
+end
